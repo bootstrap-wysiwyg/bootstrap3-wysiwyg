57534da1e3e657bc5931fa66d2f9387ea0b010a6
Christian Sterzl <christian.sterzl@gmail.com>
2013-11-22 11:24:45 +0100

    Build to copy updated locales to dist.Updated parser rules so that tests are working.

--- dist/locales/bootstrap-wysihtml5.es-ES.js
+++ dist/locales/bootstrap-wysihtml5.es-ES.js
@@ -7,7 +7,11 @@
               normal: "Texto normal",
               h1: "Título 1",
               h2: "Título 2",
-              h3: "Título 3"
+              h3: "Título 3",
+              h4: "Título 4",
+              h5: "Título 5",
+              h6: "Título 6"
+
         },
         emphasis: {
               bold: "Negrita",
