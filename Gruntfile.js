5c216c3d26b3c693b940a9bc759e316d89cf5ab9
Christian Sterzl <christian.sterzl@gmail.com>
2013-11-22 17:19:32 +0100

    Added handlebars. Cleaned up code. Bumped version to 0.2.0 because its incompatible to 0.1.0.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,31 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    handlebars: {
+      compile: {
+        options: {
+          namespace: 'wysihtml5.tpl',
+          processName: function(filePath) {
+            return filePath.split('/')[2].split('.')[0];
+          },
+          node: true
+        },
+        files: {
+          'src/templates.js': ['src/templates/*.hbs']
+        }
+      }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
         files: {
-          'dist/bootstrap3-wysihtml5.min.js': ['src/bootstrap3-wysihtml5.js']
+          'dist/bootstrap3-wysihtml5.min.js': [
+            'src/templates.js', 
+            'src/bootstrap3-wysihtml5.js',
+            'src/locales/bootstrap-wysihtml5.en-US.js'
+          ]
         }
       }
     },
@@ -26,7 +44,9 @@ module.exports = function(grunt) {
     copy: {
       main: {
         files: [
-          {expand: true, cwd: 'src/', src: ['**'], dest: 'dist/'},
+          {expand: true, cwd: 'src/', src: ['*.js'], dest: 'dist/'},
+          {expand: true, cwd: 'src/', src: ['*.css'], dest: 'dist/'},
+          {expand: true, cwd: 'src/', src: ['locales/*.js'], dest: 'dist/'},
         ]
       }
     }
@@ -36,8 +56,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-handlebars');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'cssmin', 'copy']);
+  grunt.registerTask('default', ['handlebars', 'uglify', 'cssmin', 'copy']);
 
 };
