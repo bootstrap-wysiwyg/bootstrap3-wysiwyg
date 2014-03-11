53c9cd00f412362147a1af90839a6ba7fa1b8d9c
Christian Sterzl <christian.sterzl@gmail.com>
2014-03-11 08:36:37 +0100

    Pushing v0.2.10-alpha.1

--- dist/bootstrap3-wysihtml5.js
+++ dist/bootstrap3-wysihtml5.js
@@ -11,9 +11,75 @@
   'use strict';
   var bsWysihtml5 = function($, wysihtml5) {
 
+<<<<<<< HEAD
     var templates = function(key, locale, options) {
       if(wysihtml5.tpl[key]) {
         return wysihtml5.tpl[key]({locale: locale, options: options});
+=======
+  var templates = function(key, locale, options) {
+    if(wysihtml5.tpl[key]) {
+      return wysihtml5.tpl[key]({locale: locale, options: options});
+    }
+  };
+
+  var Wysihtml5 = function(el, options) {
+    this.el = el;
+    var toolbarOpts = $.extend(true, {}, defaultOptions, options);
+    //extend shortcuts instead of overwriting em
+    $.extend(toolbarOpts.shortcuts, defaultOptions.shortcuts);
+    for(var t in toolbarOpts.customTemplates) {
+      wysihtml5.tpl[t] = toolbarOpts.customTemplates[t];
+    }
+    this.toolbar = this.createToolbar(el, toolbarOpts);
+    this.editor =  this.createEditor(toolbarOpts);
+
+    window.editor = this.editor;
+
+    $('iframe.wysihtml5-sandbox').each(function(i, el){
+      $(el.contentWindow).off('focus.wysihtml5').on({
+        'focus.wysihtml5' : function(){
+          $('li.dropdown').removeClass('open');
+        }
+      });
+    });
+  };
+
+  Wysihtml5.prototype = {
+
+    constructor: Wysihtml5,
+
+    createEditor: function(options) {
+      options = options || {};
+
+      // Add the toolbar to a clone of the options object so multiple instances
+      // of the WYISYWG don't break because 'toolbar' is already defined
+      options = $.extend(true, {}, options);
+      options.toolbar = this.toolbar[0];
+
+      var editor = new wysihtml5.Editor(this.el[0], options);
+
+      // #30 - body is in IE 10 not created by default, which leads to nullpointer
+      this.addMoreShortcuts(editor, editor.currentView.iframe.contentDocument.body || editor.currentView.iframe.contentDocument, options.shortcuts);    
+      
+      if(options && options.events) {
+        for(var eventName in options.events) {
+          editor.on(eventName, options.events[eventName]);
+        }
+      }
+      return editor;
+    },
+
+    createToolbar: function(el, options) {
+      var self = this;
+      var toolbar = $('<ul/>', {
+        'class' : 'wysihtml5-toolbar',
+        'style': 'display:none'
+      });
+      var culture = options.locale || defaultOptions.locale || 'en';
+      if(!locale.hasOwnProperty(culture)) {
+        console.debug('Locale \'' + culture + '\' not found. Available locales are: ' + Object.keys(locale) + '. Falling back to \'en\'.');
+        culture = 'en';
+>>>>>>> Pushing v0.2.10-alpha.1
       }
     };
 
@@ -165,7 +231,39 @@
 
         this.el.before(toolbar);
 
+<<<<<<< HEAD
         return toolbar;
+=======
+  var defaultOptions = $.fn.wysihtml5.defaultOptions = {
+    'font-styles': true,
+    'color': false,
+    'emphasis': true,
+    'blockquote': true,
+    'lists': true,
+    'html': false,
+    'link': true,
+    'image': true,
+    'smallmodals': false,
+    events: {},
+    parserRules: {
+      classes: {
+        'wysiwyg-color-silver' : 1,
+        'wysiwyg-color-gray' : 1,
+        'wysiwyg-color-white' : 1,
+        'wysiwyg-color-maroon' : 1,
+        'wysiwyg-color-red' : 1,
+        'wysiwyg-color-purple' : 1,
+        'wysiwyg-color-fuchsia' : 1,
+        'wysiwyg-color-green' : 1,
+        'wysiwyg-color-lime' : 1,
+        'wysiwyg-color-olive' : 1,
+        'wysiwyg-color-yellow' : 1,
+        'wysiwyg-color-navy' : 1,
+        'wysiwyg-color-blue' : 1,
+        'wysiwyg-color-teal' : 1,
+        'wysiwyg-color-aqua' : 1,
+        'wysiwyg-color-orange' : 1
+>>>>>>> Pushing v0.2.10-alpha.1
       },
 
       addMoreShortcuts: function(editor, el, shortcuts) {
--- src/generated/templates.js
+++ src/generated/templates.js
@@ -234,6 +234,7 @@ function program3(depth0,data) {
   return buffer;
   }
 
+<<<<<<< HEAD
 function program5(depth0,data) {
   
   
@@ -249,6 +250,11 @@ function program7(depth0,data) {
   buffer += "<li>\n  <div class=\"bootstrap-wysihtml5-insert-image-modal modal fade\" data-wysihtml5-dialog=\"insertImage\">\n    <div class=\"modal-dialog ";
   stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.options)),stack1 == null || stack1 === false ? stack1 : stack1.toolbar)),stack1 == null || stack1 === false ? stack1 : stack1.smallmodals), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
+=======
+  buffer += "<li>\n  <div class=\"bootstrap-wysihtml5-insert-image-modal modal fade\">\n    <div class=\"modal-dialog ";
+  stack2 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0.options)),stack1 == null || stack1 === false ? stack1 : stack1.smallmodals), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+>>>>>>> Pushing v0.2.10-alpha.1
   buffer += "\">\n      <div class=\"modal-content\">\n        <div class=\"modal-header\">\n          <a class=\"close\" data-dismiss=\"modal\">&times;</a>\n          <h3>"
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.locale)),stack1 == null || stack1 === false ? stack1 : stack1.image)),stack1 == null || stack1 === false ? stack1 : stack1.insert)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</h3>\n        </div>\n        <div class=\"modal-body\">\n          <div class=\"form-group\">\n            <input value=\"http://\" class=\"bootstrap-wysihtml5-insert-image-url form-control\" data-wysihtml5-dialog-field=\"src\">\n          </div> \n        </div>\n        <div class=\"modal-footer\">\n          <a class=\"btn btn-default\" data-dismiss=\"modal\" data-wysihtml5-dialog-action=\"cancel\" href=\"#\">"
@@ -256,8 +262,13 @@ function program7(depth0,data) {
     + "</a>\n          <a class=\"btn btn-primary\" data-dismiss=\"modal\"  data-wysihtml5-dialog-action=\"save\" href=\"#\">"
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.locale)),stack1 == null || stack1 === false ? stack1 : stack1.image)),stack1 == null || stack1 === false ? stack1 : stack1.insert)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</a>\n        </div>\n      </div>\n    </div>\n  </div>\n  <a class=\"btn ";
+<<<<<<< HEAD
   stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.options)),stack1 == null || stack1 === false ? stack1 : stack1.toolbar)),stack1 == null || stack1 === false ? stack1 : stack1.size), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
+=======
+  stack2 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0.options)),stack1 == null || stack1 === false ? stack1 : stack1.size), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+>>>>>>> Pushing v0.2.10-alpha.1
   buffer += " btn-default\" data-wysihtml5-command=\"insertImage\" title=\""
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.locale)),stack1 == null || stack1 === false ? stack1 : stack1.image)),stack1 == null || stack1 === false ? stack1 : stack1.insert)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\" tabindex=\"-1\">\n    ";
@@ -286,6 +297,7 @@ function program3(depth0,data) {
   return buffer;
   }
 
+<<<<<<< HEAD
 function program5(depth0,data) {
   
   
@@ -301,6 +313,11 @@ function program7(depth0,data) {
   buffer += "<li>\n  <div class=\"bootstrap-wysihtml5-insert-link-modal modal fade\" data-wysihtml5-dialog=\"createLink\">\n    <div class=\"modal-dialog ";
   stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.options)),stack1 == null || stack1 === false ? stack1 : stack1.toolbar)),stack1 == null || stack1 === false ? stack1 : stack1.smallmodals), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
+=======
+  buffer += "<li>\n  <div class=\"bootstrap-wysihtml5-insert-link-modal modal fade\">\n    <div class=\"modal-dialog ";
+  stack2 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0.options)),stack1 == null || stack1 === false ? stack1 : stack1.smallmodals), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+>>>>>>> Pushing v0.2.10-alpha.1
   buffer += "\">\n      <div class=\"modal-content\">\n        <div class=\"modal-header\">\n          <a class=\"close\" data-dismiss=\"modal\">&times;</a>\n          <h3>"
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.locale)),stack1 == null || stack1 === false ? stack1 : stack1.link)),stack1 == null || stack1 === false ? stack1 : stack1.insert)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</h3>\n        </div>\n        <div class=\"modal-body\">\n          <div class=\"form-group\">\n            <input value=\"http://\" class=\"bootstrap-wysihtml5-insert-link-url form-control\" data-wysihtml5-dialog-field=\"href\">\n          </div> \n          <div class=\"checkbox\">\n            <label> \n              <input type=\"checkbox\" class=\"bootstrap-wysihtml5-insert-link-target\" checked>"
@@ -310,8 +327,13 @@ function program7(depth0,data) {
     + "</a>\n          <a href=\"#\" class=\"btn btn-primary\" data-dismiss=\"modal\" data-wysihtml5-dialog-action=\"save\">"
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.locale)),stack1 == null || stack1 === false ? stack1 : stack1.link)),stack1 == null || stack1 === false ? stack1 : stack1.insert)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</a>\n        </div>\n      </div>\n    </div>\n  </div>\n  <a class=\"btn ";
+<<<<<<< HEAD
   stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.options)),stack1 == null || stack1 === false ? stack1 : stack1.toolbar)),stack1 == null || stack1 === false ? stack1 : stack1.size), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
+=======
+  stack2 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0.options)),stack1 == null || stack1 === false ? stack1 : stack1.size), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+>>>>>>> Pushing v0.2.10-alpha.1
   buffer += " btn-default\" data-wysihtml5-command=\"createLink\" title=\""
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.locale)),stack1 == null || stack1 === false ? stack1 : stack1.link)),stack1 == null || stack1 === false ? stack1 : stack1.insert)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\" tabindex=\"-1\">\n    ";
