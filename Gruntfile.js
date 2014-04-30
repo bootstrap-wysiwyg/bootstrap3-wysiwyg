3330ebecde8331d5b041666242d5d5ba4f2e56cb
Christian Sterzl <christian.sterzl@gmail.com>
2014-04-30 10:29:06 +0200

    Updated to wysihtml5x 0.4.4. Some tests still failing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
             'src/locales/bootstrap-wysihtml5.en-US.js'
           ],
           'dist/bootstrap3-wysihtml5.all.min.js': [
-            'components/wysihtml5x/dist/wysihtml5x-0.4.3-toolbar.js',
+            'components/wysihtml5x/dist/wysihtml5x-toolbar.js',
             'components/handlebars/handlebars.runtime.min.js',
             'src/generated/templates.js', 
             'src/bootstrap3-wysihtml5.js',
@@ -77,7 +77,7 @@ module.exports = function(grunt) {
       },
       all: {
         src: [
-          'components/wysihtml5x/dist/wysihtml5x-0.4.3-toolbar.js',
+          'components/wysihtml5x/dist/wysihtml5x-toolbar.js',
           'components/handlebars/handlebars.runtime.min.js',
           'src/generated/templates.js', 
           'src/bootstrap3-wysihtml5.js',
@@ -99,7 +99,7 @@ module.exports = function(grunt) {
     },
     wrap: {
       wysihtml5: {
-        src: ['components/wysihtml5x/dist/wysihtml5x-0.4.3-toolbar.js'], 
+        src: ['components/wysihtml5x/dist/wysihtml5x-toolbar.js'], 
         dest: 'dist/amd/wysihtml5.js',
         options: {
           wrapper: ['define(\'wysihtml5\', function (require, exports, module) {\n', '\nreturn wysihtml5;\n});']
