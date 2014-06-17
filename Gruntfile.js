074a311452f4c3604005452b59b3b0c9b41d2c08
Christian Sterzl <christian.sterzl@gmail.com>
2014-06-17 07:57:28 +0200

    Updated grunt contribs

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           processName: function(filePath) {
             return filePath.split('/')[2].split('.')[0];
           },
-          node: true
+          node: false
         },
         files: {
           'src/generated/templates.js': ['src/templates/*.hbs']
@@ -19,7 +19,10 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        compress: {
+          drop_console: true
+        }
       },
       build: {
         files: {
@@ -30,13 +33,16 @@ module.exports = function(grunt) {
             'src/locales/bootstrap-wysihtml5.en-US.js'
           ],
           'dist/bootstrap3-wysihtml5.all.min.js': [
-            'components/wysihtml5/dist/wysihtml5-0.3.0.min.js',
+            'components/wysihtml5x/dist/wysihtml5x-toolbar.js',
             'components/handlebars/handlebars.runtime.min.js',
             'src/generated/templates.js', 
             'src/bootstrap3-wysihtml5.js',
             'src/generated/commands.js', 
             'src/locales/bootstrap-wysihtml5.en-US.js'
-          ]          
+          ],
+          'dist/amd/bootstrap3-wysihtml5.all.min.js': [
+            'dist/amd/bootstrap3-wysihtml5.all.js'
+          ]
         }
       }
     },
@@ -57,6 +63,11 @@ module.exports = function(grunt) {
           {expand: true, cwd: 'src/', src: ['*.css'], dest: 'dist/'},
           {expand: true, cwd: 'src/', src: ['locales/*.js'], dest: 'dist/'},
         ]
+      },
+      amd: {
+        files: [
+          {expand: true, cwd: 'components/handlebars', src: ['handlebars.runtime.amd.js'], dest: 'dist/amd'}
+        ]
       }
     },
     concat: {
@@ -67,6 +78,53 @@ module.exports = function(grunt) {
         src: ['src/commands/small.js'],
         dest: 'src/generated/commands.js',
       },
+      all: {
+        src: [
+          'components/wysihtml5x/dist/wysihtml5x-toolbar.js',
+          'components/handlebars/handlebars.runtime.min.js',
+          'src/generated/templates.js', 
+          'src/bootstrap3-wysihtml5.js',
+          'src/generated/commands.js', 
+          'src/locales/bootstrap-wysihtml5.en-US.js'
+        ],
+        dest: 'dist/bootstrap3-wysihtml5.all.js'
+      },
+      amd: {
+        src: [
+          'dist/amd/wysihtml5.js',
+          'dist/amd/handlebars.runtime.amd.js',
+          'dist/amd/templates.js',
+          'dist/amd/commands.js',
+          'src/bootstrap3-wysihtml5.js'
+        ],
+        dest: 'dist/amd/bootstrap3-wysihtml5.all.js'
+      }
+    },
+    wrap: {
+      wysihtml5: {
+        src: ['components/wysihtml5x/dist/wysihtml5x-toolbar.js'], 
+        dest: 'dist/amd/wysihtml5.js',
+        options: {
+          /*jshint multistr: true */
+          wrapper: ['define(\'wysihtml5\', function (require, exports, module) {\n\
+            var $     = require(\'jquery\'),\n\
+                rangy = require(\'rangy\');\n', '\nreturn wysihtml5;\n});']
+        }
+      },
+      templates: {
+        src: ['src/generated/templates.js'],
+        dest: 'dist/amd/templates.js',
+        options: {
+          wrapper: ['define("bootstrap.wysihtml5.templates", ["handlebars.runtime", "wysihtml5"], function(HandlebarsEnv, wysihtml5) {\n\tthis["wysihtml5"] = wysihtml5;\n\tvar Handlebars = HandlebarsEnv.default;\n', '\n});']
+        }
+      },
+      commands: {
+        src: ['src/generated/commands.js'],
+        dest: 'dist/amd/commands.js',
+        options: {
+          wrapper: ['define("bootstrap.wysihtml5.commands", ["wysihtml5"], function(wysihtml5) {\n', '\n});']
+        }
+      }
     }
   });
 
@@ -76,9 +134,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('grunt-wrap');
 
   // Default task(s).
   grunt.registerTask('dev', ['handlebars', 'concat:commands']);
-  grunt.registerTask('default', ['handlebars', 'concat:commands', 'uglify', 'cssmin', 'copy']);
+  grunt.registerTask('amd', ['concat:all', 'wrap:wysihtml5', 'wrap:templates', 'wrap:commands', 'copy:amd', 'concat:amd']);
+  grunt.registerTask('default', ['handlebars:compile', 'concat:commands', 'amd', 'uglify', 'cssmin', 'copy:main']);
 
 };
