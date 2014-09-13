cca34e27824d3826c6e2e1b4bd24cdf07ac40f41
Christian Sterzl <christian.sterzl@gmail.com>
2014-09-13 20:37:24 +0200

    Resolves #89

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['qunit'],
     plugins: ['karma-qunit', 
+              'karma-coverage',
               'karma-chrome-launcher',
               'karma-phantomjs-launcher'],
 
@@ -58,14 +59,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+      'src/**/*.js': 'coverage' 
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
